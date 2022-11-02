21a855626db9023a7a03c80367f646f7152285de
Mykola Pichunov <91594379+Pichunov-N@users.noreply.github.com>
2022-11-02 15:01:20 +0200

    created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
+import { JobListCardComponent } from './job-list/job-list-card/job-list-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent,
+    JobDetailComponent,
+    JobListCardComponent
   ],
   imports: [
     BrowserModule,
